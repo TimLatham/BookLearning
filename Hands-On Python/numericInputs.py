2b80f64c5a539bfeb5640ee98d72cdfaff3e20c5
Tim Latham <lathamtk@yahoo.com>
2017-03-01 16:09:59 -0700

    feat: Load updated numericInputs file
    Added feature functionality featuring multiple assignment addition
    and quotients for the exercises.

--- Hands-On Python/numericInputs.py
+++ Hands-On Python/numericInputs.py
@@ -17,4 +17,15 @@ As a test, run the program entering the expressions 5-1 and 2*3.
 
 x = input("Enter a number: ")
 y = input("Enter a second number: ")
+z, a = input("Enter two comma separated numbers: ")
+b, c, d = input("Enter three comma separated numbers: ")
+
 print 'The sum of %s and %s is %s.' % (x, y, x+y)
+print 'The sum of %s and %s is %s.' % (z, a, z+a)
+print 'The sum of %s and %s and %s is %s.' % (b, c, d, b+c+d)
+
+e, f = input("Enter two comma separated numbers, the first to be divided by the second: ")
+print 'The quotient of %s and %s is %s with a remainder of %s' % (e, f, e/f, e%f)
+quotient = e/f
+remainder = e%f
+print 'The quotient of %s and %s is %s with a remainder of %s' % (e, f, quotient, remainder)
